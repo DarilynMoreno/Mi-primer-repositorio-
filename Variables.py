f1127a6580dfe2eba31d1e2b9f87c836b578f8f5
DarilynMoreno <darilynmoreno1803@gmail.com>
2026-06-04 17:32:56 -0500

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -1,1 +1,33 @@
+#Esto es un comentario de una sola linea 
+"""Esto es un comentario de
+varias lineas"""
 
+#IniciaLizando variables
+nombre="Nombre del estudiante"
+edad=58
+estado=True
+nota=5.0
+
+#mostrar el contenido de las variales print()
+print(nombre)
+print(edad)
+print(estado)
+print(nota)
+
+#Que tipo de dato contiene cada variable.
+print(type(nombre))
+print(type(edad))
+print(type(estado))
+primt(type(nota))
+
+#Vamls a utilizar la función input para reciger datos por medio del teclado.
+nombre=input("¿Como te llamas?")
+edad=input("¿Que esad tienes?")
+estado=input("¿En que estado de encuentras?")
+nota=input("¿cual estu nota?")
+
+#Para visualizar que guardamos en las variables anteriores
+print("Hola,",nombre,"un gusto conocerte")
+print("Tu edad es:",edad)
+print("Tu estado es:",estado)
+print("Tu nota es:",nota)
